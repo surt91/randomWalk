80f851f96d71712fcf7feccc3a3f4e91ec9f6e1f
Hendrik Schawe <surt91gmail.com>
2016-02-26 17:15:52 +0100

    tclap
    https://sourceforge.net/projects/tclap/

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,48 @@
+#include <string>
+
+#include <tclap/CmdLine.h>
+
+#include "Walker.hpp"
+#include "RNG.hpp"
+
 int main(int argc, char** argv)
 {
+    // TCLAP throws exceptions
+    try{
+        // Usage, delimiter, version
+        TCLAP::CmdLine cmd("Calculates random walks", ' ', "0.1");
 
+        // value argument
+        // -short, --long, description, required, default, type
+        TCLAP::ValueArg<int> numArg("n", "number", "how many steps", true, 100, "integer");
+        TCLAP::ValueArg<int> seedArg("x", "seed", "seed for rng", false, 0, "integer");
+        TCLAP::ValueArg<std::string> svgArg("s", "svg", "svg filename", false, "", "string");
+
+        // switch argument
+        // -short, --long, description, default
+//        TCLAP::SwitchArg reverseSwitch("r","reverse","Print name backwards", false);
+
+        // Add to the parser
+        cmd.add(numArg);
+        cmd.add(seedArg);
+        cmd.add(svgArg);
+//        cmd.add(reverseSwitch);
+        // Parse the argv array.
+        cmd.parse( argc, argv );
+
+        // Get the value parsed by each arg.
+        std::string svg_path = svgArg.getValue();
+        int n = numArg.getValue();
+        int seed = seedArg.getValue();
+
+        std::vector<double> numbers = rng(n, seed);
+        Walker w(2, numbers);
+        w.print();
+        if(!svg_path.empty())
+            w.svg(svg_path);
+    }
+    catch(TCLAP::ArgException &e)  // catch any exceptions
+    {
+        std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+    }
 }
