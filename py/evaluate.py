a872e15bca746a50473dcec573e20214b32c49e6
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-04-06 16:24:14 +0200

    adaptive noise filter ... not perfect ... needs work

--- py/evaluate.py
+++ py/evaluate.py
@@ -21,27 +21,30 @@ def getDistribution(infile, outfile, theta, steps):
     col = param.parameters["observable"]
     counts, bins = np.histogram(a.transpose()[col], 50, density=True)
     centers = (bins[1:] + bins[:-1])/2
+    centers_err = [(centers[i] - bins[i]) / 2 for i in range(len(centers))]
 
+    # errors are not good, we need to include the autocorrelation time
     bs_mean, bs_err = bootstrap_histogram(a.transpose()[col], bins=bins, density=True)
     #bs_err = histogram_simple_error(counts)
 
-    # only for uniform bins
-    center_err = (bins[1] - bins[0]) / 4
     print(outfile)
 
     with open(outfile, "w") as f:
         f.write("# steps S S_err P(S) P(S)_err\n")
-        for s, pts, pts_err in zip(centers, bs_mean, bs_err):
+        for s, s_err, pts, pts_err in zip(centers, centers_err, bs_mean, bs_err):
             #~ print(i, exp(i/theta) * j)
             try:
                 ps = s/theta + log(pts)
                 # gaussian erroro propagation
-                ps_err = 1/pts * pts_err + center_err/theta
+                ps_err = 1/pts * pts_err + s_err/theta
+                if ps_err > s_err:
+                    # ignore too noisy bins
+                    raise ValueError
             except ValueError:
                 ps = 0
             else:
-                f.write("{} {} {} {} {}\n".format(steps, s, center_err, ps, ps_err))
-                #f.write("{} {} {} {} {}\n".format(steps, i, center_err, j, j_err))
+                f.write("{} {} {} {} {}\n".format(steps, s, s_err, ps, ps_err))
+                #f.write("{} {} {} {} {}\n".format(steps, s, s_err, pts, pts_err))
 
 
 def run():
