36310dcc47e6410e8ba8ecd28161e0b864859175
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-04-07 13:16:35 +0200

    ignore parts of the histogram with bad statistics and warn the user

--- py/evaluate.py
+++ py/evaluate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from math import exp, log, ceil
+import logging
 
 import numpy as np
 from scipy.interpolate import interp1d
@@ -9,30 +10,36 @@ import parameters as param
 from config import bootstrap, bootstrap_histogram, histogram_simple_error
 
 
+logging.basicConfig(level=logging.INFO,
+                format='%(asctime)s -- %(levelname)s :: %(message)s',
+                datefmt='%d.%m.%YT%H:%M:%S')
+logging.info("started")
+
+
 def getAutocorrTime(data):
     # TODO: calculate the autocorrelation time
     autocorr = np.correlate(data-np.mean(data), data-np.mean(data), mode='full')[len(data)-1:]
     x0 = autocorr[0]
 
-    # integrate only up to the first negative values
-    #~ m = -1
-    #~ for n, i in enumerate(autocorr):
-        #~ if i < 0:
-            #~ m = n
-            #~ break
+    #~ # integrate only up to the first negative values
+    m = -1
+    for n, i in enumerate(autocorr):
+        if i < 0:
+            m = n
+            break
 
-    tau = sum(autocorr)/x0
-    print("autocorrelation time:", tau)
+    tau = sum(autocorr[:m])/x0
+    logging.info("autocorrelation time: " + str(tau))
     return tau
 
-def getDistribution(infile, outfile, theta, steps):
+def getDistribution(infile, outfile, histfile, theta, steps, num_bins=50):
     try:
         a = np.loadtxt(infile+".gz")
     except FileNotFoundError:
         try:
             a = np.loadtxt(infile)
         except FileNotFoundError:
-            print("cannot find " + self.filename)
+            logging.error("cannot find " + self.filename)
             return
 
     col = param.parameters["observable"]
@@ -42,36 +49,42 @@ def getDistribution(infile, outfile, theta, steps):
     # do only keep statistically independent samples to not underestimate the error
     data = data[::ceil(2*t_corr)]
 
-    counts, bins = np.histogram(data, 50, density=True)
+
+    # histograms do not need to be normed, this will happen in the end
+    counts, bins = np.histogram(data, num_bins)
     centers = (bins[1:] + bins[:-1])/2
     centers_err = [(centers[i] - bins[i]) / 2 for i in range(len(centers))]
 
-    bs_mean, bs_err = bootstrap_histogram(data, bins=bins, density=True)
+    bs_mean, bs_err = bootstrap_histogram(data, bins=bins)
     #bs_err = histogram_simple_error(counts)
 
-    data = []
+    data_p = []
     for s, s_err, pts, pts_err in zip(centers, centers_err, bs_mean, bs_err):
         #~ print(i, exp(i/theta) * j)
         try:
             ps_log = s/theta + log(pts)
             # gaussian erroro propagation
             ps_log_err = pts_err/pts  + s_err/theta
-            if pts_err/pts > s_err/theta:
-                # ignore too noisy bins
-                #~ raise ValueError
+            if pts < 10:
+                # ignore bin with too few entries
+                raise ValueError
                 pass
         except ValueError:
             pass
         else:
-            data.append((s, s_err, ps_log, ps_log_err))
+            data_p.append((s, s_err, ps_log, ps_log_err))
 
-    print(outfile)
+    logging.info(outfile)
     with open(outfile, "w") as f:
         f.write("# S S_err P(S) P(S)_err\n")
-        for d in data:
+        for d in data_p:
+            f.write("{} {} {} {}\n".format(*d))
+    with open(histfile, "w") as f:
+        f.write("# S S_err P(S) P(S)_err\n")
+        for d in zip(centers, centers_err, bs_mean, bs_err):
             f.write("{} {} {} {}\n".format(*d))
 
-    return data
+    return data_p
 
 
 def getZtheta(list_of_ps_log):
@@ -82,11 +95,21 @@ def getZtheta(list_of_ps_log):
         l2 = list_of_ps_log[i+1]
 
         # calculate a cubic spline through l1
-        x, x_err, y, y_err = zip(*l1)
-        spline_1 = interp1d(x, y, kind='cubic')
+        try:
+            x, x_err, y, y_err = zip(*l1)
+            spline_1 = interp1d(x, y, kind='cubic')
+        except ValueError:
+            logging.error("too few samples")
+            Ztheta_mean.append((0,0))
+            continue
 
         # calculate for every s in l2 the difference to l2(s) - spline_l1(s)
         Z = [ps_log - spline_1(s) for s, _, ps_log, _ in l2 if min(x) < s < max(x)]
+
+        # not enough overlap
+        if len(Z) < 5:
+            logging.warning("not enough overlap, insert an intermediate theta")
+
         Ztheta_mean.append(bootstrap(Z))
 
     return Ztheta_mean
@@ -118,7 +141,10 @@ def run():
                                          theta=T,
                                          **param.parameters
                                          )
-            data = getDistribution("{}/{}.dat".format(d, name), "{}/dist_{}.dat".format(out, name), T, N)
+            data = getDistribution("{}/{}.dat".format(d, name),
+                                   "{}/dist_{}.dat".format(out, name),
+                                   "{}/hist_{}.dat".format(out, name),
+                                   T, N)
             list_of_ps_log.append(data)
             z = getZtheta(list_of_ps_log)
             #~ outfiles.append('"{}/dist_{}.dat"'.format(out, name))
