72454ab4a6d6c734c428014b1003703eec5cbefa
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-04-13 17:03:50 +0200

    fixed same rng seed in case of seed of 0

--- src/WangLandau.cpp
+++ src/WangLandau.cpp
@@ -73,7 +73,8 @@ void WangLandau::run()
     {
         // rngs should be local to the threads, with different seeds
         // FIXME: think about a better seed
-        UniformRNG rngMC(o.seedMC * (omp_get_thread_num()+1));
+        int id = omp_get_thread_num();
+        UniformRNG rngMC((o.seedMC+id) * (id+1));
 
         std::unique_ptr<Walker> w;
         prepare(w);
