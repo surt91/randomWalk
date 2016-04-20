2cc449d8b4b9229b7e7a1f3cf8eec929122413c9
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-04-20 17:43:28 +0200

    decluttered Header, more readable

--- src/Step.hpp
+++ src/Step.hpp
@@ -18,162 +18,37 @@ namespace std {
 template<class T>
 class Step
 {
-    friend struct std::hash<Step>;
+    friend struct std::hash<Step<T>>;
 
     public:
-        Step()
-        {};
-
-        Step(int d, double rn)
-              : m_d(d)
-        {
-            m_coordinates = std::vector<int>(d, 0);
-            for(int i=0; i<d; ++i)
-                if(rn * d < i+1) // direction i
-                {
-                    if(rn * d - i < 0.5)
-                        m_coordinates[i] = 1;
-                    else
-                        m_coordinates[i] = -1;
-
-                    break;
-                }
-        };
-
+        Step(){};
+        Step(int d, double rn);
         Step(const std::vector<T> &coord)
               : m_d(coord.size()),
                 m_coordinates(coord)
-              {};
+        {};
 
-        double length() const
-        {
-            double s=0;
-            for(const auto i : m_coordinates)
-                s += i*i;
+        // properties
+        double length() const;
+        double angle(int i=0, int j=1) const;
 
-            return std::sqrt(s);
-        }
-
-        double angle(int i=0, int j=1) const //only 2D projection on axis i and j
-        {
-            return atan2(m_coordinates[j], m_coordinates[i]);
-        }
-
+        // comparison operators
         template <class U>
         friend inline bool operator==(const Step<U> &lhs, const Step<U> &rhs);
 
         template <class U>
         friend inline bool operator!=(const Step<U> &lhs, const Step<U> &rhs);
 
-        Step<T> operator+(const Step<T> &other) const
-        {
-            if(m_d != other.d())
-                throw std::invalid_argument("dimensions do not agree");
+        bool operator<(const Step<T> &other) const;
 
-            std::vector<T> new_coord = other.coordinates();
-            for(int i=0; i<m_d; ++i)
-                new_coord[i] += m_coordinates[i];
-
-            return Step<T>(new_coord);
-        }
-
-        Step<T> operator-(const Step<T> &other) const
-        {
-            if(m_d != other.d())
-                throw std::invalid_argument("dimensions do not agree");
-
-            std::vector<T> new_coord = other.coordinates();
-            for(int i=0; i<m_d; ++i)
-                new_coord[i] -= m_coordinates[i];
-
-            return Step<T>(new_coord);
-        }
-
-        Step<T> operator-() const
-        {
-            std::vector<T> new_coord = coordinates();
-            for(int i=0; i<m_d; ++i)
-                new_coord[i] *= -1;
-
-            return Step<T>(new_coord);
-        }
-
-        Step<T> operator/(const double d) const
-        {
-            std::vector<T> new_coord = coordinates();
-            for(int i=0; i<m_d; ++i)
-                new_coord[i] /= d;
-
-            return Step<T>(std::move(new_coord));
-        }
-
-        Step<T>& operator+=(const Step<T> &other)
-        {
-            if(m_d != other.d())
-                throw std::invalid_argument("dimensions do not agree");
-
-            const std::vector<T> other_coord = other.coordinates();
-            for(int i=0; i<m_d; ++i)
-                m_coordinates[i] += other_coord[i];
-
-            return *this;
-        }
-
-        Step<T>& operator-=(const Step<T> &other)
-        {
-            if(m_d != other.d())
-                throw std::invalid_argument("dimensions do not agree");
-
-            const std::vector<T> other_coord = other.coordinates();
-            for(int i=0; i<m_d; ++i)
-                m_coordinates[i] -= other_coord[i];
-
-            return *this;
-        }
-
-        Step& operator/=(const double d)
-        {
-            // uncool, since my coordinates are integer
-            for(int i=0; i<m_d; ++i)
-                m_coordinates[i] = std::round(m_coordinates[i]/d);
-
-            return *this;
-        }
-
-        // a point is smaller, if the x value is smaller
-        // in case of a tie the smaller y value decides
-        // only for 2d, since it is only needed for the 2d andrew algorithm
-        bool operator <(const Step<T> &other) const
-        {
-            return x() < other.x() || (x() == other.x() && y() < other.y());
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const Step<T> &obj)
-        {
-            os << "(";
-            for(auto i : obj.m_coordinates)
-                os << i << ", ";
-            os << ") ";
-            return os;
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const std::vector<Step<T>> &obj)
-        {
-            os << "[";
-            for(const auto &i : obj)
-                os << i << " ";
-            os << "]";
-            return os;
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const std::list<Step<T>> &obj)
-        {
-            os << "[";
-            for(const auto &i : obj)
-                os << i << " ";
-            os << "]";
-            return os;
-        }
+        // arithmetic operators
+        Step<T> operator+(const Step<T> &other) const;
+        Step<T> operator-(const Step<T> &other) const;
+        Step<T> operator-() const;
+        Step<T> operator/(const double d) const;
+        Step<T>& operator+=(const Step<T> &other);
+        Step<T>& operator-=(const Step<T> &other);
+        Step<T>& operator/=(const double d);
 
         template <class U>
         friend Step<U> cross(const Step<U> &a, const Step<U> &b);
@@ -181,6 +56,17 @@ class Step
         template <class U>
         friend double dot(const Step<U> &a, const Step<U> &b);
 
+        // output operators
+        template <class U>
+        friend std::ostream& operator<<(std::ostream& os, const Step<U> &obj);
+
+        template <class U>
+        friend std::ostream& operator<<(std::ostream& os, const std::vector<Step<U>> &obj);
+
+        template <class U>
+        friend std::ostream& operator<<(std::ostream& os, const std::list<Step<U>> &obj);
+
+        // access operators
         int operator[](std::size_t idx) const { return m_coordinates[idx]; };
         int& operator[](std::size_t idx) { return m_coordinates[idx]; };
 
@@ -201,6 +87,39 @@ class Step
 };
 
 template <class T>
+Step<T>::Step(int d, double rn)
+      : m_d(d)
+{
+    m_coordinates = std::vector<int>(d, 0);
+    for(int i=0; i<d; ++i)
+        if(rn * d < i+1) // direction i
+        {
+            if(rn * d - i < 0.5)
+                m_coordinates[i] = 1;
+            else
+                m_coordinates[i] = -1;
+
+            break;
+        }
+}
+
+template <class T>
+double Step<T>::length() const
+{
+    double s=0;
+    for(const auto i : m_coordinates)
+        s += i*i;
+
+    return std::sqrt(s);
+}
+
+template <class T>
+double Step<T>::angle(int i, int j) const //only 2D projection on axis i and j
+{
+    return atan2(m_coordinates[j], m_coordinates[i]);
+}
+
+template <class T>
 bool operator==(const Step<T> &lhs, const Step<T> &rhs)
 {
     if(lhs.m_d != rhs.m_d)
@@ -217,6 +136,97 @@ template <class T>
 bool operator!=(const Step<T> &lhs, const Step<T> &rhs)
 {
     return ! (lhs == rhs);
+}
+
+template <class T>
+Step<T> Step<T>::operator+(const Step<T> &other) const
+{
+    if(m_d != other.d())
+        throw std::invalid_argument("dimensions do not agree");
+
+    std::vector<T> new_coord = other.coordinates();
+    for(int i=0; i<m_d; ++i)
+        new_coord[i] += m_coordinates[i];
+
+    return Step<T>(new_coord);
+}
+
+template <class T>
+Step<T> Step<T>::operator-(const Step<T> &other) const
+{
+    if(m_d != other.d())
+        throw std::invalid_argument("dimensions do not agree");
+
+    std::vector<T> new_coord = other.coordinates();
+    for(int i=0; i<m_d; ++i)
+        new_coord[i] -= m_coordinates[i];
+
+    return Step<T>(new_coord);
+}
+
+template <class T>
+Step<T> Step<T>::operator-() const
+{
+    std::vector<T> new_coord = coordinates();
+    for(int i=0; i<m_d; ++i)
+        new_coord[i] *= -1;
+
+    return Step<T>(new_coord);
+}
+
+template <class T>
+Step<T> Step<T>::operator/(const double d) const
+{
+    std::vector<T> new_coord = coordinates();
+    for(int i=0; i<m_d; ++i)
+        new_coord[i] /= d;
+
+    return Step<T>(std::move(new_coord));
+}
+
+template <class T>
+Step<T>& Step<T>::operator+=(const Step<T> &other)
+{
+    if(m_d != other.d())
+        throw std::invalid_argument("dimensions do not agree");
+
+    const std::vector<T> other_coord = other.coordinates();
+    for(int i=0; i<m_d; ++i)
+        m_coordinates[i] += other_coord[i];
+
+    return *this;
+}
+
+template <class T>
+Step<T>& Step<T>::operator-=(const Step<T> &other)
+{
+    if(m_d != other.d())
+        throw std::invalid_argument("dimensions do not agree");
+
+    const std::vector<T> other_coord = other.coordinates();
+    for(int i=0; i<m_d; ++i)
+        m_coordinates[i] -= other_coord[i];
+
+    return *this;
+}
+
+template <class T>
+Step<T>& Step<T>::operator/=(const double d)
+{
+    // uncool, since my coordinates are integer
+    for(int i=0; i<m_d; ++i)
+        m_coordinates[i] = std::round(m_coordinates[i]/d);
+
+    return *this;
+}
+
+// a point is smaller, if the x value is smaller
+// in case of a tie the smaller y value decides
+// only for 2d, since it is only needed for the 2d andrew algorithm
+template <class T>
+bool Step<T>::operator<(const Step<T> &other) const
+{
+    return x() < other.x() || (x() == other.x() && y() < other.y());
 }
 
 template <class T>
@@ -246,6 +256,36 @@ double dot(const Step<T> &a, const Step<T> &b)
         p += a[i]*b[i];
 
     return p;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Step<T> &obj)
+{
+    os << "(";
+    for(auto i : obj.m_coordinates)
+        os << i << ", ";
+    os << ") ";
+    return os;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<Step<T>> &obj)
+{
+    os << "[";
+    for(const auto &i : obj)
+        os << i << " ";
+    os << "]";
+    return os;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::list<Step<T>> &obj)
+{
+    os << "[";
+    for(const auto &i : obj)
+        os << i << " ";
+    os << "]";
+    return os;
 }
 
 namespace std {
