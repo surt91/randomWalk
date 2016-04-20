ebba71f095c1d3cd939cf1da5cfe07ce49a93f06
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-04-20 10:29:31 +0200

    fewer instances for equilibration
    since they are trapped at A=0 for T>0

--- src/Metropolis.cpp
+++ src/Metropolis.cpp
@@ -14,9 +14,9 @@ int Metropolis::equilibrate(std::unique_ptr<Walker>& w1, UniformRNG& rngMC1)
 
     RollingMean rmean1(samples);
     RollingMean rmean2(samples);
-    RollingMean rmean3(samples);
-    RollingMean rmean4(samples);
-    RollingMean rmean5(samples);
+    //~ RollingMean rmean3(samples);
+    //~ RollingMean rmean4(samples);
+    //~ RollingMean rmean5(samples);
 
     UniformRNG rngMC((o.seedMC+1)*0x243F6A8885A3);
 
@@ -26,17 +26,17 @@ int Metropolis::equilibrate(std::unique_ptr<Walker>& w1, UniformRNG& rngMC1)
     prepare(w2);
     w2->degenerateMaxVolume();
 
-    std::unique_ptr<Walker> w3;
-    prepare(w3);
-    w3->degenerateMaxSurface();
+    //~ std::unique_ptr<Walker> w3;
+    //~ prepare(w3);
+    //~ w3->degenerateMaxSurface();
 
-    std::unique_ptr<Walker> w4;
-    prepare(w4);
-    w4->degenerateSpiral();
+    //~ std::unique_ptr<Walker> w4;
+    //~ prepare(w4);
+    //~ w4->degenerateSpiral();
 
-    std::unique_ptr<Walker> w5;
-    prepare(w5);
-    w5->degenerateStraight();
+    //~ std::unique_ptr<Walker> w5;
+    //~ prepare(w5);
+    //~ w5->degenerateStraight();
 
     // FIXME: this is redundant code, needs to be cleaned up
     while(true)
@@ -48,48 +48,49 @@ int Metropolis::equilibrate(std::unique_ptr<Walker>& w1, UniformRNG& rngMC1)
             // save the random number before the change
             double oldS1 = S(w1);
             double oldS2 = S(w2);
-            double oldS3 = S(w3);
-            double oldS4 = S(w4);
-            double oldS5 = S(w5);
+            //~ double oldS3 = S(w3);
+            //~ double oldS4 = S(w4);
+            //~ double oldS5 = S(w5);
             w1->change(rngMC1);
             w2->change(rngMC);
-            w3->change(rngMC);
-            w4->change(rngMC);
-            w5->change(rngMC);
+            //~ w3->change(rngMC);
+            //~ w4->change(rngMC);
+            //~ w5->change(rngMC);
 
             if(!o.simpleSampling)
             {
                 // Metropolis rejection
                 double p_acc1 = std::min({1.0, exp(-(S(w1) - oldS1)/o.theta)});
                 double p_acc2 = std::min({1.0, exp(-(S(w2) - oldS2)/o.theta)});
-                double p_acc3 = std::min({1.0, exp(-(S(w3) - oldS3)/o.theta)});
-                double p_acc4 = std::min({1.0, exp(-(S(w4) - oldS4)/o.theta)});
-                double p_acc5 = std::min({1.0, exp(-(S(w5) - oldS5)/o.theta)});
+                //~ double p_acc3 = std::min({1.0, exp(-(S(w3) - oldS3)/o.theta)});
+                //~ double p_acc4 = std::min({1.0, exp(-(S(w4) - oldS4)/o.theta)});
+                //~ double p_acc5 = std::min({1.0, exp(-(S(w5) - oldS5)/o.theta)});
                 if(p_acc1 < 1 - rngMC1())
                     w1->undoChange();
                 if(p_acc2 < 1 - rngMC())
                     w2->undoChange();
-                if(p_acc3 < 1 - rngMC())
-                    w3->undoChange();
-                if(p_acc4 < 1 - rngMC())
-                    w4->undoChange();
-                if(p_acc5 < 1 - rngMC())
-                    w5->undoChange();
+                //~ if(p_acc3 < 1 - rngMC())
+                    //~ w3->undoChange();
+                //~ if(p_acc4 < 1 - rngMC())
+                    //~ w4->undoChange();
+                //~ if(p_acc5 < 1 - rngMC())
+                    //~ w5->undoChange();
             }
         }
 
         if(Logger::verbosity >= LOG_INFO)
             oss << t_eq << " " << w1->L() << " " << w1->A()
                         << " " << w2->L() << " " << w2->A()
-                        << " " << w3->L() << " " << w3->A()
-                        << " " << w4->L() << " " << w4->A()
-                        << " " << w5->L() << " " << w5->A() << std::endl;
+                        //~ << " " << w3->L() << " " << w3->A()
+                        //~ << " " << w4->L() << " " << w4->A()
+                        //~ << " " << w5->L() << " " << w5->A()
+                        << std::endl;
 
         rmean1.add(S(w1));
         rmean2.add(S(w2));
-        rmean3.add(S(w3));
-        rmean4.add(S(w4));
-        rmean5.add(S(w5));
+        //~ rmean3.add(S(w3));
+        //~ rmean4.add(S(w4));
+        //~ rmean5.add(S(w5));
 
         // FIXME: this is a bit complicated, can it be simplified?
         // wait until the variance does not fluctuate more than "threshold" (eg. 10%) within w1
@@ -100,15 +101,17 @@ int Metropolis::equilibrate(std::unique_ptr<Walker>& w1, UniformRNG& rngMC1)
 
         LOG(LOG_TOO_MUCH) << t_eq << " " << start << " "
                           << (std::abs(rmean1.mean() - rmean2.mean()) < sdev)
-                          << " " << (std::abs(rmean1.mean() - rmean3.mean()) < sdev)
-                          << " " << (std::abs(rmean1.mean() - rmean4.mean()) < sdev)
-                          << " " << (std::abs(rmean1.mean() - rmean5.mean()) < sdev);
+                          //~ << " " << (std::abs(rmean1.mean() - rmean3.mean()) < sdev)
+                          //~ << " " << (std::abs(rmean1.mean() - rmean4.mean()) < sdev)
+                          //~ << " " << (std::abs(rmean1.mean() - rmean5.mean()) < sdev)
+                          ;
 
         if(start
           && std::abs(rmean1.mean() - rmean2.mean()) < sdev
-          && std::abs(rmean1.mean() - rmean3.mean()) < sdev
-          && std::abs(rmean1.mean() - rmean4.mean()) < sdev
-          && std::abs(rmean1.mean() - rmean5.mean()) < sdev)
+          //~ && std::abs(rmean1.mean() - rmean3.mean()) < sdev
+          //~ && std::abs(rmean1.mean() - rmean4.mean()) < sdev
+          //~ && std::abs(rmean1.mean() - rmean5.mean()) < sdev
+          )
             break;
         ++t_eq;
     }
