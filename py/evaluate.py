5daa6bc2a05c05926d38c6e5c197afe042f78606
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-06-20 10:58:42 +0200

    added "fastmode" to not recalculate histograms
    mainly useful for tweaking "S" for the rate function extrapolation

--- py/evaluate.py
+++ py/evaluate.py
@@ -8,26 +8,29 @@ import evaluateWangLandau
 import commonEvaluation
 
 if __name__ == "__main__":
-    sampling = param.parameters["sampling"]
-    if sampling == 1:
-        if "--lin" in sys.argv:
-            ht = 1
-            logging.info("Using equi-spaced histogram")
-        elif "--log" in sys.argv:
-            ht = 2
-            logging.info("Using logarithmic histogram")
-        elif "--flat" in sys.argv:
-            ht = 3
-            logging.info("Using percentile-based histogram")
-        else:
-            logging.info("Default: Using log-spaced histogram")
-            ht = 2
-        if not "--fast" in sys.argv:
-            evaluateMetropolis.run(ht)
-        else:
-            logging.info("Fastmode: do not create new histograms from rawData")
-    elif sampling == 2 or sampling == 3:
-        evaluateWangLandau.run()
+    if not "--fast" in sys.argv:
+        sampling = param.parameters["sampling"]
+        if sampling == 1:
+            if "--lin" in sys.argv:
+                ht = 1
+                logging.info("Using equi-spaced histogram")
+            elif "--log" in sys.argv:
+                ht = 2
+                logging.info("Using logarithmic histogram")
+            elif "--flat" in sys.argv:
+                ht = 3
+                logging.info("Using percentile-based histogram")
+            else:
+                logging.info("Default: Using log-spaced histogram")
+                ht = 2
+
+            evaluateMetropolis(ht).run()
+
+        elif sampling == 2 or sampling == 3:
+            evaluateWangLandau.run()
+
+    else:
+        logging.info("Fastmode: do not create new histograms from rawData")
 
     commonEvaluation.cut_trans(param.parameters["S"])
     commonEvaluation.get_max_dist()
