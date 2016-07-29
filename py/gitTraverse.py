8fd76bdb7f5cb9b7038c94176ccd76bcad815bc2
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-07-29 16:23:36 +0200

    __main__ guard

--- py/gitTraverse.py
+++ py/gitTraverse.py
@@ -19,57 +19,58 @@ import binascii
 
 import git
 
-cloned_path = os.path.join(tmp_path, 'tmp_benchmark')
-repo = git.Repo(repo_path)
-try:
-    cloned_repo = repo.clone(cloned_path)
-except git.exc.GitCommandError:
-    pass
+if __name__ == "__main__":
+    cloned_path = os.path.join(tmp_path, 'tmp_benchmark')
+    repo = git.Repo(repo_path)
+    try:
+        cloned_repo = repo.clone(cloned_path)
+    except git.exc.GitCommandError:
+        pass
 
-pwd = os.getcwd()
-os.chdir(cloned_path)
-os.system("git checkout master")
-os.system("git submodule init")
-os.system("git submodule update")
-os.system("make clean")
-os.system("git pull origin master")
-os.chdir("src")
-
-dates = []
-shas = []
-times = []
-with open(os.path.join(pwd, "gitBenchmark.dat"), "w") as f:
-    f.write("# %s\n" % cmd)
-    f.write("# unixtime commit benchmarktime message\n")
-
-N = -1
-for n, c in enumerate(repo.iter_commits('master', max_count=N)):
-    # set HEAD to the current commit
-    print("{}/{}".format(n, N))
-    sha = binascii.hexlify(c.binsha).decode("ascii")
-    print("git checkout {}".format(sha))
-    os.system("git checkout {}".format(sha))
-    os.system("make")
-    start = time.time()
-    e = os.system(cmd)
-    duration = time.time() - start
+    pwd = os.getcwd()
+    os.chdir(cloned_path)
+    os.system("git checkout master")
+    os.system("git submodule init")
+    os.system("git submodule update")
     os.system("make clean")
-    # program exits with errorcode -> do not use the measured time
-    if e != 0:
-        duration = float("nan")
-    print(duration)
-    times.append(duration)
-    dates.append(c.committed_date)
-    shas.append(sha)
+    os.system("git pull origin master")
+    os.chdir("src")
 
-    with open(os.path.join(pwd, "gitBenchmark.dat"), "a") as f:
-        f.write('{} {} {} "{}"\n'.format(c.committed_date, sha, duration, c.message.split("\n")[0]))
+    dates = []
+    shas = []
+    times = []
+    with open(os.path.join(pwd, "gitBenchmark.dat"), "w") as f:
+        f.write("# %s\n" % cmd)
+        f.write("# unixtime commit benchmarktime message\n")
 
-for i, j, k in zip(dates, shas, times):
-    print("{} {} {}".format(i, j, k))
+    N = -1
+    for n, c in enumerate(repo.iter_commits('master', max_count=N)):
+        # set HEAD to the current commit
+        print("{}/{}".format(n, N))
+        sha = binascii.hexlify(c.binsha).decode("ascii")
+        print("git checkout {}".format(sha))
+        os.system("git checkout {}".format(sha))
+        os.system("make")
+        start = time.time()
+        e = os.system(cmd)
+        duration = time.time() - start
+        os.system("make clean")
+        # program exits with errorcode -> do not use the measured time
+        if e != 0:
+            duration = float("nan")
+        print(duration)
+        times.append(duration)
+        dates.append(c.committed_date)
+        shas.append(sha)
 
-print("plot in gnuplot with: \n"
-      "set timefmt '%s'\n"
-      "set format x '%m/%d/%Y %H:%M:%S'\n"
-      "set xdata time\n"
-      "p 'gitBench.dat' u 1:3 w l t '{}', '' u 1:3:4 with labels rotate left offset 0,char 1".format(cmd))
+        with open(os.path.join(pwd, "gitBenchmark.dat"), "a") as f:
+            f.write('{} {} {} "{}"\n'.format(c.committed_date, sha, duration, c.message.split("\n")[0]))
+
+    for i, j, k in zip(dates, shas, times):
+        print("{} {} {}".format(i, j, k))
+
+    print("plot in gnuplot with: \n"
+          "set timefmt '%s'\n"
+          "set format x '%m/%d/%Y %H:%M:%S'\n"
+          "set xdata time\n"
+          "p 'gitBench.dat' u 1:3 w l t '{}', '' u 1:3:4 with labels rotate left offset 0,char 1".format(cmd))
