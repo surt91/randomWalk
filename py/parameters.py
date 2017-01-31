b649bbeee4799ccd5042894376c5b423ca341464
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2017-01-31 11:50:48 +0100

    example parameter file for FWLE sampling

--- py/parameters.py
+++ py/parameters.py
@@ -6,55 +6,28 @@ basetheta = basename + "_T{theta:.5f}"
 basee = basename + "_e{estart:.0f}-{eend:.0f}"
 
 sizes = (32, 64, 128, 256, 512, 1024, 2048)
-sweep = {n: n**0.5 for n in sizes}
 
 # thetas for the system sizes, missing sizes will get the 0 entry
-# dict{size: list[theta]}
 thetas = {
-      32: (1, 10, float("inf"), -10, -5, -4, -3, -2, -1),
-      64: (1, 3,  10, float("inf"), -20, -13, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1),
-    1024: (1, 3,  10, float("inf"), -20, -13, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1),
-}
+         }
 
-# dict{size: dict{theta: t_eq}}
 t_eq = {
-     512: {-50: 8e4},
-    1024: {T: 10000 for T in thetas[1024]}
-}
+       }
 
-# dict{size: dict{theta: t_corr}}
 t_corr = {
-      64: {-10: 2.0, -15: 1.7},
-     128: {-20: 4.5, 5: 2.3, -50: 1.7, -15:3.8, -30: 1.9, -5: 3, -10: 3.6},
-     256: {10: 2.1, -200: 1.7, -30: 6.2, -10: 2.7, -450: 1.3, -400: 2.1, -370: 2.5, -50: 4.4},
-     512: {-1500: 2.2, -1700: 2.2, -1600: 4.4, -1800: 1.4, -400: 10.4, -300: 8.4},
-    1024: {-6500: 5.3, -6300: 15.9, -6400: 15.9, -6350: 15.9, -6000: 1.9, -1600: 11.7, -1500: 5.9},
-}
+         }
 
-# target for rate function: Phi_target -> max_energy = Phi_target*N**d
-# dict{size: list[energy_borders]}
-energies = {
-      32: np.linspace(   10,     10000,  6),
-      64: np.linspace(   20,     80000, 12),
-     128: np.linspace(  800,   1000000, 24),
-     256: np.linspace( 5000,  12000000, 48),
-     384: np.linspace( 9000,  62000000, 48),
-     512: list(np.linspace(   12000,    100000, 10, endpoint=False))
-        + list(np.linspace(  100000,    160000, 10, endpoint=False))
-        + list(np.linspace(  160000,   1600000, 10, endpoint=False))
-        + list(np.linspace( 1600000,  16000000, 10, endpoint=False))
-        + list(np.linspace(16000000, 132000000, 40)),
-    1024: list(np.linspace(   24000,    400000, 10, endpoint=False))
-        + list(np.linspace(  400000,    600000, 10, endpoint=False))
-        + list(np.linspace(  600000,   6000000, 10, endpoint=False))
-        + list(np.linspace( 6000000,  60000000, 10, endpoint=False))
-        + list(np.linspace(60000000, 932000000, 40)),
-    2048: list(np.linspace(    48000,    1800000, 10, endpoint=False))
-        + list(np.linspace(  1800000,    2800000, 10, endpoint=False))
-        + list(np.linspace(  2800000,   28000000, 10, endpoint=False))
-        + list(np.linspace( 28000000,  280000000, 10, endpoint=False))
-        + list(np.linspace(280000000, 5932000000, 40)),
-}
+energies = {    32: [1, 20, 40, 80, 200, 400, 1000, 1500],
+                64: [1, 20, 40, 80, 200, 400, 1000, 2500, 6000],
+               128: [2, 20, 40, 80, 200, 400, 1000, 2500, 6000, 12000, 25000],
+               256: [8, 20, 40, 80, 200, 400, 1000, 2500, 6000, 12000, 25000, 60000],
+               512: [15, 40, 80, 200, 400, 1000, 2500, 6000, 12000, 25000, 60000, 120000],
+              1024: [25, 40, 80, 200, 400, 1000, 2500, 6000, 12000, 25000, 60000, 120000, 240000],
+              2048: [50, 80, 200, 400, 1000, 2500, 6000, 12000, 25000, 60000, 120000, 240000, 480000],
+              2048: [50, 80, 200, 400, 1000, 2500, 6000, 12000, 25000, 60000, 120000, 240000, 480000],
+           }
+
+#S = [0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
 parameters = {
     # what type
@@ -74,19 +47,14 @@ parameters = {
     # dimension
     "dimension": 2,
 
-    # number of walkers
-    "number_of_walkers": 1,
     # list of lengths of the walks
     "number_of_steps": sizes,
-    # number of MC tries per sweep
-    "sweep": sweep,
 
     # dict[N] of different thetas
     "thetas": thetas,
 
     # dict[N][theta] of equilibration times
     "t_eq": t_eq,
-    "t_eq_max": 1e4,
 
     # dict[N][theta] of estimated autocorrelation times
     # will be used to take by factor t_corr more samples
@@ -97,8 +65,7 @@ parameters = {
 
     "nbins": 100,
     "overlap": 20,
-    "overlap_direction": "right",
-    "lnf": 1e-8,
+    "lnf": 1e-5,
     "flatness": 0.8,
 
     # where to save the temporary evaluation results
@@ -109,14 +76,15 @@ parameters = {
     "rawConf": False,
 
     # for which observable wen want the probabilities
-    "observable": 2,  # 1: surface area, 2: volume
+    # 1: surface area
+    # 2: volume
+    # 3: passage times
+    "observable": 2,
 
     # algorithm for the convex hull
     # qhull seems to be fastest, at least for big number of steps
     # for small number of steps, andrews is faster
-    # 1: qhull,
-    # 2: andrews (only 2D)
-    "method": 2,
+    "method": 2,  # 1: qhull, 2: andrews, 4: jarvis
 
     # akl heuristic
     # only available in d=2, yet
@@ -124,9 +92,9 @@ parameters = {
     # which smapling methd
     # 1: Metropolis
     # 2: Wang Landau
-    # 3: Fast 1/t Wang Landau
-    # 4: Metropolis with parallel tempering
-    "sampling": 2,
+    # 3: fast Wang Landau and Entropic sampling
+    # 4: parallel tempering
+    "sampling": 3,
 
     # how many cpus (only for wang landau), None means all
     # for HERO: 1 - 12
@@ -138,7 +106,17 @@ parameters = {
     "basee": basee,
 
     # parameter for evaluation of the rate function
-    "S":     [0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
-    # N to begin the fit (one entry per value of S)
-    "N_min": [   0,    0,    0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
+    "S":     [0.02, 0.03, 0.035, 0.045, 0.05, 0.06, 0.08, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0],
+    "N_min": [ 256,  128,   128,   128,    0,    0,    0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
+
+    # range for the gaussian fit to the peak on a rescaled axis
+    "fit_xrange": {
+                        32: (0.5, 1.5),
+                        64: (0.5, 1.5),
+                       128: (0.7, 1.5),
+                       256: (0.7, 1.7),
+                       512: (0.7, 1.7),
+                      1024: (0.8, 1.6),
+                      2048: (0.9, 1.5),
+                  },
 }
