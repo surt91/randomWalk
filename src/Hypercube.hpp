6287d238dae31da7619d95c86cb9a0f040b747b8
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2017-08-23 17:15:40 +0200

    hypercube is now a template

--- src/Hypercube.hpp
+++ src/Hypercube.hpp
@@ -15,7 +15,8 @@ class Hypercube
     public:
         Hypercube() {}
 
-        bool bestfs(Step<int> source, Step<int> target, std::unordered_set<Step<int>>& occupied);
+        template<class T>
+        bool bestfs(Step<int> source, Step<int> target, T& occupied);
 
 };
 
@@ -28,3 +29,43 @@ struct thingy {
         return key > other.key;
     };
 };
+
+template<class T>
+bool Hypercube::bestfs(Step<int> source, Step<int> target, T& occupied)
+{
+    std::priority_queue<thingy> q;
+    std::unordered_set<Step<int>> visited;
+
+    q.emplace(source, target.dist(source));
+
+    while(!q.empty())
+    {
+        Step<int> c = q.top().value;
+        q.pop();
+
+        double D = target.dist(c);
+        if(c == target)
+        {
+            return true;
+        }
+        for(auto i : c.neighbors())
+        {
+            if(!visited.count(i) && !occupied.count(i))
+            {
+                visited.insert(i);
+                double d = target.dist(i);
+                if(d < D)
+                {
+                    q.emplace(c, D);
+                    q.emplace(i, d);
+                    continue;
+                }
+                else
+                {
+                    q.emplace(i, d);
+                }
+            }
+        }
+    }
+    return false;
+}
