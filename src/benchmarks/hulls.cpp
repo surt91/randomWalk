764e1fad2f2bf00c699805d9cc3c8f8a3104f881
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2017-10-04 17:31:50 +0200

    convex hull benchmarks

---
+++ src/benchmarks/hulls.cpp
@@ -0,0 +1,33 @@
+#include <benchmark/benchmark.h>
+
+#include "../Cmd.hpp"
+#include "../walker/Walker.hpp"
+#include "../walker/LatticeWalker.hpp"
+#include "../simulation/Simulation.hpp"
+
+template <class ...ExtraArgs>
+void BM_convex_hull_algorithm(benchmark::State& state, hull_algorithm_t type, int d=2) {
+    Cmd o;
+
+    o.d = d;
+    o.steps = state.range(0);
+    o.type = WT_RANDOM_WALK;
+
+    o.chAlg = CH_NOP;
+
+    std::unique_ptr<Walker> w;
+    Simulation::prepare(w, o);
+
+    while (state.KeepRunning())
+        w->setHullAlgo(type);
+}
+BENCHMARK_CAPTURE(BM_convex_hull_algorithm, Andrews, CH_ANDREWS)->Arg(512)->Arg(2048);
+BENCHMARK_CAPTURE(BM_convex_hull_algorithm, qhull, CH_QHULL)->Arg(512)->Arg(2048);
+BENCHMARK_CAPTURE(BM_convex_hull_algorithm, Jarvis, CH_JARVIS)->Arg(512)->Arg(2048);
+
+BENCHMARK_CAPTURE(BM_convex_hull_algorithm, AndrewsAkl, CH_ANDREWS_AKL)->Arg(512)->Arg(2048);
+BENCHMARK_CAPTURE(BM_convex_hull_algorithm, qhullAkl, CH_QHULL_AKL)->Arg(512)->Arg(2048);
+BENCHMARK_CAPTURE(BM_convex_hull_algorithm, JarvisAkl, CH_JARVIS_AKL)->Arg(512)->Arg(2048);
+
+BENCHMARK_CAPTURE(BM_convex_hull_algorithm, qhull3d, CH_QHULL, 3)->Arg(512)->Arg(2048);
+BENCHMARK_CAPTURE(BM_convex_hull_algorithm, qhullAkl3d, CH_QHULL_AKL, 3)->Arg(512)->Arg(2048);
---
+++ src/benchmarks/step.cpp
@@ -0,0 +1,31 @@
+#include <benchmark/benchmark.h>
+
+#include "Step.hpp"
+
+static void BM_neighbors2(benchmark::State& state) {
+    Step<int> a {13, 42};
+    while (state.KeepRunning())
+        a.neighbors();
+}
+BENCHMARK(BM_neighbors2);
+
+static void BM_neighbors_diag2(benchmark::State& state) {
+    Step<int> a {13, 42};
+    while (state.KeepRunning())
+        a.neighbors(true);
+}
+BENCHMARK(BM_neighbors_diag2);
+
+static void BM_neighbors3(benchmark::State& state) {
+    Step<int> a {13, 42, 23};
+    while (state.KeepRunning())
+        a.neighbors();
+}
+BENCHMARK(BM_neighbors3);
+
+static void BM_neighbors_diag3(benchmark::State& state) {
+    Step<int> a {13, 42, 23};
+    while (state.KeepRunning())
+        a.neighbors(true);
+}
+BENCHMARK(BM_neighbors_diag3);
