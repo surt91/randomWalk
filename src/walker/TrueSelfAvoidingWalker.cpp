74e39258039dd95d0c4fe0358230457a9e0945de
Hendrik Schawe <surt91@gmail.com>
2018-04-24 11:40:08 +0200

    initial untested TSAW implementation
    The bias function to avoid visited fields is not correct yet

---
+++ src/walker/TrueSelfAvoidingWalker.cpp
@@ -0,0 +1,94 @@
+#include "TrueSelfAvoidingWalker.hpp"
+
+TrueSelfAvoidingWalker::TrueSelfAvoidingWalker(int d, int numSteps, const UniformRNG &rng_in, hull_algorithm_t hull_algo, bool amnesia)
+    : SpecWalker<int>(d, numSteps, rng_in, hull_algo, amnesia)
+{
+
+}
+
+/// Get new random numbers and reconstruct the walk
+void TrueSelfAvoidingWalker::reconstruct()
+{
+
+}
+
+void TrueSelfAvoidingWalker::updateSteps()
+{
+    std::unordered_map<Step<int>, int> number_of_visits;
+
+    // build the whole walk here
+
+    // probabilities to step on the neighboring sites
+    std::vector<double> p(2*d);
+    // current position
+    Step<int> head(d);
+
+    m_steps[0].fillFromRN(random_numbers[0]);
+    for(int t=1; t<numSteps; ++t)
+    {
+        // iterate over neighbors, to update the probabilites p
+        int ctr = 0;
+        double norm = 0.;
+        std::vector<Step<int>> neighbors = m_steps[t-1].neighbors();
+        for(const auto &i : neighbors)
+        {
+            int times_visited = number_of_visits[i];
+            double prob = std::exp(beta*times_visited); // FIXME: correct formula
+            norm += prob;
+            p[ctr] = norm; // p is cumulative probability function
+            ++ctr;
+        }
+
+        // step on a neighbor according to p
+        double rn = random_numbers[t] * norm;
+        int idx = 0;
+        // for high dimensions a bisection would make sense, but for low not
+        while(rn > p[idx])
+        {
+            ++idx;
+        }
+        m_steps[t] = neighbors[idx];
+    }
+}
+
+/** Changes the walk, i.e., performs one trial move.
+ *
+ * \param rng Random number generator to draw the needed randomness from
+ * \param update Should the hull be updated after the change?
+ */
+void TrueSelfAvoidingWalker::change(UniformRNG &rng, bool update)
+{
+    // I should do this in a far more clever way
+    int idx = rng() * nRN();
+    undo_index = idx;
+    undo_value = random_numbers[idx];
+    random_numbers[idx] = rng();
+
+    newStep.fillFromRN(random_numbers[idx]);
+    // test if something changes
+    undoStep.fillFromRN(undo_value);
+    if(newStep == undoStep)
+        return;
+
+    updateSteps();
+    updatePoints();
+
+    if(update)
+    {
+        m_old_convex_hull = m_convex_hull;
+        updateHull();
+    }
+}
+
+/// Undoes the last change.
+void TrueSelfAvoidingWalker::undoChange()
+{
+    random_numbers[undo_index] = undo_value;
+    // test if something changed
+    if(newStep == undoStep)
+        return;
+
+    updateSteps();
+    updatePoints();
+    m_convex_hull = m_old_convex_hull;
+}
---
+++ src/walker/TrueSelfAvoidingWalker.hpp
@@ -0,0 +1,39 @@
+#ifndef SELFAVOIDINGWALKER_H
+#define SELFAVOIDINGWALKER_H
+
+#include <list>
+#include <iterator>
+#include <unordered_set>
+
+#include "../Logging.hpp"
+#include "SpecWalker.hpp"
+
+/** True Self-Avoiding Random Walk
+ *
+ * A Walk which tries not to self intersect.
+ *
+ * See also:
+ * doi: 10.1103/PhysRevB.27.1635
+ *
+ * \image html TSAW.svg "example of a true self-avoiding walk"
+ */
+class TrueSelfAvoidingWalker final : public SpecWalker<int>
+{
+    public:
+        TrueSelfAvoidingWalker(int d, int numSteps, const UniformRNG &rng, hull_algorithm_t hull_algo, bool amnesia=false);
+
+        void reconstruct() final;
+
+        void updateSteps() final;
+
+        void change(UniformRNG &rng, bool update=true) final;
+        void undoChange() final;
+
+        double beta;
+
+    protected:
+        Step<int> newStep;
+        Step<int> undoStep;
+};
+
+#endif
